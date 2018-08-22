1d8cc3217b51e4cb6cdc984d202de752ea591843
Michael Williams <michaelsaircraft@gmail.com>
2018-08-22 11:18:37 -0700

    updated code to troubleshoot geoQuery.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="hello">
-    <button @click="geoTest">Launch Script</button>
+    <button v-on:click="loadLocations">+1000 documents to collection</button>
+    <button v-on:click="findPoints">Find Points</button>
+    <p>{{ this.clientPosition }}</p>
+
+    <div v-if="true">
+      <ul v-for="location in locations" :key=location.g>
+        <li class="location">
+          <p>{{ location.d }}</p>
+          <p>{{ location.g }}</p>
+
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
 import 'firebase/firestore' //if use firestore
-import GeoFirestore from 'geofirestore'
+import { GeoFirestore } from 'geofirestore'
+//import axios from 'axios'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "xxxx",
-    authDomain: "xxxx",
-    databaseURL: "xxxx",
-    projectId: "xxxx",
-    storageBucket: "xxxx",
-    messagingSenderId: "xxxx"
+    apiKey: "XXXX",
+    authDomain: "XXXX",
+    databaseURL: "XXXX",
+    projectId: "XXXX",
+    storageBucket: "XXXX",
+    messagingSenderId: "XXXX"
   })
 }
 
@@ -24,25 +38,101 @@ firebase.firestore().settings({ timestampsInSnapshots: true })
 const db = firebase.firestore()
 
 // Create a Firebase reference where GeoFirestore will store its information
-const collectionRef = firebase.firestore().collection('geotest');
+const collectionRef = firebase.firestore().collection('geo_test');
 
 // Create a GeoFirestore index
 const geoFirestore = new GeoFirestore(collectionRef);
-
 export default {
-
+  data() {
+    return {
+      clientPosition:{
+        lat: null,
+        lng: null
+      },
+      locations: []
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  beforeMount: function() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          console.log(`here is the position lat: ${position.coords.latitude}`)
+          console.log(`here is the position lng: ${position.coords.longitude}`)
+          this.clientPosition.lat = position.coords.latitude
+          this.clientPosition.lng = position.coords.longitude
+        })
+      } else {
+        alert('Sorry, this browser does not support location, or you have blocked location from Scout Military.')
+      }
+
+    fetch('http://ip-api.com/json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      this.clientPosition.lat = myJson.lat
+      this.clientPosition.lng = myJson.lon
+    });
+
+  },
   methods: {
-    geotest (evt) {
+    setLocation () {
+      
+    },
+    loadLocations (evt) {
       evt.preventDefault()
-      geoFirestore.add({ coordinates: new firebase.firestore.GeoPoint(37.79, -122.41)}).then((docRef) => {
+      for(let i = 0; i < 1000; i ++){
+        // place random geopoints around your area within a range of ~500 miles (+-7 degrees)
+        let rand_lat = (Math.random() * 14) - 7
+        let rand_lng = (Math.random() * 14) - 7
+        geoFirestore.add({ 
+        coordinates: new firebase.firestore.GeoPoint(this.clientPosition.lat + rand_lat, this.clientPosition.lng + rand_lng)
+        }).then((docRef) => {
         console.log(docRef.id); // ID of newly added document
       }, (error) => {
         console.log('Error: ' + error);
       });
+      }
+    },
+    geoTestMethod (evt) { //adds one document to collection
+      evt.preventDefault()
+      console.log('Adding new point')
+      geoFirestore.add({ 
+        coordinates: new firebase.firestore.GeoPoint(37.79, -122.41),
+        info1:'string of info 1',
+        info2:'string of info 2',
+        info3:'string of info 3',
+        obj1:{
+          num1: 1,
+          info4:'string of info 4'
+        }
+        }).then((docRef) => {
+        console.log(docRef.id); // ID of newly added document
+      }, (error) => {
+        console.log('Error: ' + error);
+      });
+    },
+    findPoints (evt) {
+      evt.preventDefault()
+      
+      console.log('finding points...')
+      const geoQuery = geoFirestore.query({
+        center: new firebase.firestore.GeoPoint(this.clientPosition.lat, this.clientPosition.lng),
+        radius: 5.1
+        //query: (ref) => ref//.where(key<String>, '==', something<Any>)
+      });
+      console.log(`Center of query: ${JSON.stringify(geoQuery.center())}`)
+      const query = geoQuery.query();  // A query object
+      
+      query.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          //console.log(doc.data())
+          this.locations.push(doc.data())
+        })
+      })
     }
   }
 }
@@ -50,6 +140,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.location{
+  background-color: rgb(233, 233, 233);
+  margin:5px;
+  padding: 5px;
+  width: 700px;
+}
 h3 {
   margin: 40px 0 0;
 }
